e99e40084f0e58899ee8e2a970153bbeacf86c1c
sanjaykumardev <duderaj1976@gmail.com>
2023-12-12 14:24:18 +0530

    TEST

--- backend/index.js
+++ backend/index.js
@@ -6,16 +6,16 @@ const cors = require("cors")
 port = 3000
 
 app.use(express.json());
-app.use(cors({origin: "https://test-six-omega-19.vercel.app/"}));
+app.use(cors())
 
-
+// origin: "https://test-six-omega-19.vercel.app/"}));
 
 // connect to server to database 
 const connection  = mysql.createConnection({
-  host:"bhwk0kuh7dhyearjwvok-mysql.services.clever-cloud.com",
-  user:"ulgf6hscgxrztcuh",
-  password:"ddUyywwNIaBuF3WudbSW",
-  database:"bhwk0kuh7dhyearjwvok"
+  user:"root",
+  host:"localhost",
+  password:"sanjay007",
+  database:"employeeform"
 });
 
 
@@ -35,6 +35,47 @@ connection.query('INSERT INTO form ( employeenmae,dob,designation,salary,experie
    });
  });
 
+
+ app.get('/employees', (req, res) => {
+  const query = 'SELECT * FROM form'; // Replace 'employees' with your table name
+  connection.query(query, (error, results) => {
+      if (error) {
+          console.error('Error fetching employees:', error);
+          res.status(500).send('Error fetching employees');
+          return;
+      }
+      res.json(results);
+  });
+});
+
+app.put('/employees/update/:employee_id', (req, res) => {
+  const { employeenmae, dob, designation, salary, experience, phone_number, address, email } = req.body;
+  const employee_id = req.params.employee_id;
+  const query = 'UPDATE employees SET employeenmae=?, dob=?, designation=?, salary=?, experience=?, phone_number=?, address=?, email=? WHERE employee_id=?';
+  connection.query(query, [employeenmae, dob, designation, salary, experience, phone_number, address, email, employee_id], (error, results) => {
+      if (error) {
+          console.error('Error updating employee:', error);
+          res.status(500).send('Error updating employee');
+          return;
+      }
+      res.status(200).send('Employee updated successfully');
+  });
+});
+
+
+
+app.delete('/employees/delete/:employee_id', (req, res) => {
+  const employee_id = req.params.employee_id;
+  const query = 'DELETE FROM form WHERE employee_id = ?';
+  connection.query(query, [employee_id], (error, results) => {
+      if (error) {
+          console.error('Error deleting employee:', error);
+          res.status(500).send('Error deleting employee');
+          return;
+      }
+      res.status(200).send('Employee deleted successfully');
+  });
+});
 
 // connection  to database 
  connection.connect( (err)=>{
--- frontend/script.js
+++ frontend/script.js
@@ -71,3 +71,115 @@ function handleSubmit() {
    
 }
 
+
+
+// function handleSubmit() {
+//     const employeenmae = document.getElementById("employeeName").value;
+//     const dob = document.getElementById("dob").value;
+//     const designation = document.getElementById("designation").value;
+//     const salary = document.getElementById("salary").value;
+//     const experience = document.getElementById("experience").value;
+//     const employee_id = document.getElementById("employeeId").value;
+//     const phone_number = document.getElementById("phoneNumber").value;
+//     const address = document.getElementById("address").value;
+//     const email = document.getElementById("email").value;
+
+//     if (!employeenmae || !dob || !designation || !employee_id || !phone_number || !email) {
+//         alert("Please fill in all required fields.");
+//         return;
+//     }
+
+//     const URL = "http://localhost:3000";
+//     const data = {
+//         employeenmae,
+//         dob,
+//         designation,
+//         salary,
+//         experience,
+//         employee_id,
+//         phone_number,
+//         address,
+//         email
+//     };
+
+//     axios.post(  URL + '/post', data)
+//         .then(response => {
+//             console.log('Data sent to the server:', response.data);
+//         })
+//         .catch(error => {
+//             console.error('Error:', error);
+//         });
+
+//     const table = document.getElementById("employeeTable");
+//     const newRow = table.insertRow(table.length);
+
+//     const cells = [employee_id, employeenmae, dob, designation, salary, experience, phone_number, address, email];
+//     for (let i = 0; i < cells.length; i++) {
+//         const cell = newRow.insertCell(i);
+//         cell.innerHTML = cells[i];
+//     }
+
+//     resetForms();
+// }
+
+// function resetForms() {
+//     document.getElementById("employeeName").value = "";
+//     document.getElementById("dob").value = "";
+//     document.getElementById("designation").value = "";
+//     document.getElementById("salary").value = "";
+//     document.getElementById("experience").value = "";
+//     document.getElementById("employeeId").value = "";
+//     document.getElementById("phoneNumber").value = "";
+//     document.getElementById("address").value = "";
+//     document.getElementById("email").value = "";
+// }
+
+// function deleteEntry(row) {
+//     const table = document.getElementById("employeeTable");
+//     const index = row.rowIndex;
+//     table.deleteRow(index);
+
+//     const idToDelete = row.cells[0].innerHTML;
+//     axios.delete(`http://localhost:3000/delete/${idToDelete}`)
+//         .then(response => {
+//             console.log('Entry deleted successfully:', response.data);
+//         })
+//         .catch(error => {
+//             console.error('Error deleting entry:', error);
+//         });
+// }
+
+// function editEntry(row) {
+//     for (let i = 1; i < row.cells.length; i++) {
+//         row.cells[i].contentEditable = true;
+//     }
+
+//     const saveButton = document.createElement("button");
+//     saveButton.textContent = "Save";
+//     saveButton.onclick = function () {
+//         const updatedData = {
+//             employeenmae: row.cells[1].innerHTML,
+//             dob: row.cells[2].innerHTML,
+//             designation: row.cells[3].innerHTML,
+//             salary: row.cells[4].innerHTML,
+//             experience: row.cells[5].innerHTML,
+//             employee_id: row.cells[0].innerHTML,
+//             phone_number: row.cells[6].innerHTML,
+//             address: row.cells[7].innerHTML,
+//             email: row.cells[8].innerHTML
+//         };
+
+//   axios.put(`http://localhost:3000/update/${updatedData.employee_id}`, updatedData)
+//     .then(response => {
+//       console.log('Entry updated successfully:', response.data);
+//           for (let i = 1; i < row.cells.length; i++) {
+//          row.cells[i].contentEditable = false;
+//                 }
+//             })
+//             .catch(error => {
+//                 console.error('Error updating entry:', error);
+//             });
+//     };
+
+//     row.appendChild(saveButton);
+// }
